00260d4495505c786f397965e0b83333294614d8
JDeVoldemort <104032188+JDeVoldemort@users.noreply.github.com>
2024-01-19 23:18:44 -0700

    removed testing proxy data

---
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrl: './contact-detail.component.css'
+})
+export class ContactDetailComponent {
+    // contact: Contact = new Contact(
+    // );
+
+    constructor() { }
+
+}
