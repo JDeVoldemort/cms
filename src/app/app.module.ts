a4b3010161e0e569f299353de4f5a756f3a34e33
JDeVoldemort <104032188+JDeVoldemort@users.noreply.github.com>
2024-02-02 16:43:11 -0700

    finish 4 docs and dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../app/header.component';
 import { AppComponent } from './app.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactsComponent } from './contacts/contacts.component';
-import { ContactItemComponent } from './contacts/contact-item/contact-item.component';
-import { DocumentsComponent } from './documents/documents.component';
-import { DocumentListComponent } from './documents/document-list/document-list.component';
-import { DocumentItemComponent } from './documents/document-item/document-item.component';
 import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
-import { MessageItemComponent } from './messages/message-item/message-item.component';
+import { DocumentItemComponent } from './documents/document-item/document-item.component';
+import { DocumentListComponent } from './documents/document-list/document-list.component';
+import { DocumentsComponent } from './documents/documents.component';
 import { MessageEditComponent } from './messages/message-edit/message-edit.component';
+import { MessageItemComponent } from './messages/message-item/message-item.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 
 @NgModule({
@@ -31,7 +32,8 @@ import { MessageListComponent } from './messages/message-list/message-list.compo
     DocumentDetailComponent,
     MessageItemComponent,
     MessageEditComponent,
-    MessageListComponent
+    MessageListComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+    @Output() selectedDocumentEvent = new EventEmitter();
 
+    documents = [
+      new Document('1', 'Document 1', 'This is the first document', 'http://www.google.com'),
+      new Document('2', 'Document 2', 'This is the second document', 'http://www.google.com'),
+      new Document('3', 'Document 3', 'This is the third document', 'http://www.google.com'),
+      new Document('4', 'Document 4', 'This is the fourth document', 'http://www.google.com')
+
+    ]
+    onSelectedDocument(document: Document) {
+      this.selectedDocumentEvent.emit(document);
+    }
 }
